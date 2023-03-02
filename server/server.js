e797485d1a8af4fbea2adef2261a8c99b8729250
Milo Palmer <milogpalmer@gmail.com>
2023-03-03 10:26:47 +1300

    made a get route to test home page

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
 server.get('/', (req, res) => {
-  res.render('main')
+  res.render('home')
 })
 
 module.exports = server
