481f7ca9887ad8506122b6d51bd819a5e420d986
Caroline Stadnicki <carolinestadnicki.nz@gmail.com>
2023-03-03 14:56:55 +1300

    Add route to indian and vegan restaurants page

--- server/indian-routes.js
+++ server/indian-routes.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const fs = require('fs').promises
 const indianRouter = express.Router()
 
+indianRouter.get('/:id', async(req,res) => {
+    try{
+        const data = await fs.readFile('server/data/data.json', 'utf-8')
+        const cuisinesData = JSON.parse(data)
+        const indianData = cuisinesData.cuisines.find((cuisine) => {
+            if(cuisine.hasOwnProperty('indian')) {
+                return cuisine.indian[Number(req.params.id) - 1]             
+            }
+        })
+        const finalData = indianData.indian[Number(req.params.id) - 1];
+        res.render('restaurant', finalData)
+        }catch (err){
+        console.log(err)
+    }
+})
 
 module.exports = indianRouter
