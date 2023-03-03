55f065bab5e56fec86c87be80933916be35adab7
Caroline Stadnicki <carolinestadnicki.nz@gmail.com>
2023-03-03 14:27:40 +1300

    Add route for asian restaurants

--- server/asian-routes.js
+++ server/asian-routes.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const fs = require('fs').promises
 const asianRouter = express.Router()
 
+asianRouter.get('/:id', async(req,res) => {
+    try{
+        const data = await fs.readFile('server/data/data.json', 'utf-8')
+        const cuisinesData = JSON.parse(data)
+        const asianData = cuisinesData.cuisines.find((cuisine) => {
+            if(cuisine.hasOwnProperty('asian')) {
+                return cuisine.asian[Number(req.params.id) - 1]             
+            }
+        })
+        const finalData = asianData.asian[Number(req.params.id) - 1];
+        res.render('restaurant', finalData)
+        }catch (err){
+        console.log(err)
+    }
+})
 
 module.exports = asianRouter
