220cc0cc7f0ffde6e63f11d57b9a1bd335ff9b05
Caroline Stadnicki <carolinestadnicki.nz@gmail.com>
2023-03-03 14:40:11 +1300

    Add route to italian restaurants page

--- server/italian-routes.js
+++ server/italian-routes.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const fs = require('fs').promises
 const italianRouter = express.Router()
+
+italianRouter.get('/:id', async(req,res) => {
+    try{
+        const data = await fs.readFile('server/data/data.json', 'utf-8')
+        const cuisinesData = JSON.parse(data)
+        const italianData = cuisinesData.cuisines.find((cuisine) => {
+            if(cuisine.hasOwnProperty('italian')) {
+                return cuisine.italian[Number(req.params.id) - 1]             
+            }
+        })
+        const finalData = italianData.italian[Number(req.params.id) - 1];
+        res.render('restaurant', finalData)
+        }catch (err){
+        console.log(err)
+    }
+})
 
 
 module.exports = italianRouter
